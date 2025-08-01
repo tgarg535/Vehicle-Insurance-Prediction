f0ec9ed3fd57b6acf40f9b5cd00856bfe73a75bc
tgarg535 <tanushgarg26jul@gmail.com>
2025-08-02 02:23:13 +0530

    final update 2 of data ingestion

--- demo.py
+++ demo.py
@@ -21,7 +21,7 @@
 #     raise MyException(e, sys) from e
 
 # --------------------------------------------------------------------------------
-from src.pipline.training_pipeline import TrainPipeline
+# from src.pipline.training_pipeline import TrainPipeline
 
-pipline = TrainPipeline()
-pipline.run_pipeline()
+# pipline = TrainPipeline()
+# pipline.run_pipeline()
