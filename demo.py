fd90acf61d2e6321bc8677b9a30fd0839c6a2a54
tgarg535 <tanushgarg26jul@gmail.com>
2025-08-02 02:10:57 +0530

    some modifications for data ingestion

--- demo.py
+++ demo.py
@@ -21,3 +21,7 @@
 #     raise MyException(e, sys) from e
 
 # --------------------------------------------------------------------------------
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -2,8 +2,8 @@ import os
 from datetime import date
 
 # For MongoDB connection
-DATABASE_NAME = "Proj1"
-COLLECTION_NAME = "Proj1-Data"
+DATABASE_NAME = "vehicle-insurance-prediction"
+COLLECTION_NAME = "data"
 MONGODB_URL_KEY = "MONGODB_URL"
 
 PIPELINE_NAME: str = ""
